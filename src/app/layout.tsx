62993fca258cda6faf829c8cc49d988506e6d4ce
Ali Hamza Kamboh <alihamzakamboh180@gmail.com>
2024-08-27 02:06:42 +0500

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,7 +50,7 @@ export default function RootLayout({
 }) {
   return (
     <html suppressHydrationWarning lang="en">
-<script src="https://chat.ordemio.com/lib/w.js" assistant-id="93eb4b2d-b472-496f-8de3-05870122266b" async></script>
+<script src="https://chat.ordemio.com/lib/w.js" assistant-id="bd824f37-9ae8-4ca9-84da-4cd51a6c6ce8" async></script>
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
           <Header />
